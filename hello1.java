5ef1d356dfbfb48e5b1344a5c30c50425d327d94
heman yadav16 <yadavheman16@gmail.com>
2023-11-08 10:32:03 +0530

    hello file

---
+++ hello1.java
@@ -0,0 +1,13 @@
+class hello
+{
+    public static void main (String args[])
+    {
+        int num =7;
+        //num = num +1;
+        //num+=1;
+        //num++;
+        //++num;
+        int result = num++;   //fetches first and then increments next
+        int result1 = ++num;  //increments first and then fetches
+    }
+}
