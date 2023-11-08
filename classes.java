e29830e628178172534fc3f85a928397808471a3
heman yadav16 <yadavheman16@gmail.com>
2023-11-08 13:13:30 +0530

    mthods overloading

--- classes.java
+++ classes.java
@@ -29,28 +29,61 @@
 
 
 //methods
-class computer
-{
-    public void playmusic()
-    {
-        System.out.println("bhupender jogi");
+// class computer
+// {
+//     public void playmusic()
+//     {
+//         System.out.println("bhupender jogi");
     
-    }
+//     }
 
-    public String getmeapen()
+//     public String getmeapen()
+//     {
+//         return "pen";
+//     }
+// }
+
+
+// public class classes
+// {
+//     public static void main (String args[])
+//     {
+//         computer obj = new computer();
+//         obj.playmusic();
+//         String str= obj.getmeapen();
+//         System.out.println(str);
+//     }
+// }
+
+
+
+//method overloading  :  multiple methods with same name but different task 
+class calculator
+{
+    public int add (int n1,int n2,int n3)
     {
-        return "pen";
+        return n1+n2+n3 ;
+    }
+    public int add (int n1,int n2)
+    {
+        return n1+n2 ;
+
+    }
+    public double add (double n1,int n2)
+    {
+        return n1+n2 ;
     }
 }
-
 
 public class classes
 {
-    public static void main (String args[])
-    {
-        computer obj = new computer();
-        obj.playmusic();
-        String str= obj.getmeapen();
-        System.out.println(str);
+     public static void main( String args[])
+     {
+        int num1 =9;
+         int num2 =8;
+         calculator calc = new calculator();
+         int result = calc.add(num1,num2);
+         System.out.println(result);
+
+     }
     }
-}
