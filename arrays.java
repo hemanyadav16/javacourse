afcfe12da69bffd3794af909cb26a489bec92610
heman yadav16 <yadavheman16@gmail.com>
2023-11-09 10:15:54 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,14 @@
+class arrays 
+{
+    public static void main (String args[])
+    {
+        int num[]={0,3,5,9,};
+        for (int i=0;i<4;i++)
+        {
+            System.out.println(num[i]);
+        }
+
+    }
+    
+}    
+
