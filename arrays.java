1c4b1a484634a8b855ad9e8a52b33a179b352d0d
heman yadav16 <yadavheman16@gmail.com>
2023-11-09 12:43:30 +0530

    jagged arrays

--- arrays.java
+++ arrays.java
@@ -15,28 +15,61 @@
 
 // multidimensional array
 
+// class arrays
+// {
+//     public static void main (String args[])
+//     {
+//         int nums[][] = new int[3][4];
+//         for (int i=0;i<3;i++)
+//         {
+//             for (int j= 0;j<4;j++)
+//             {
+//                 nums[i][j]= (int)(Math.random() *10);
+
+//             }
+//         }
+
+//         for (int i=0;i<3;i++)
+//         {
+//             for (int j= 0;j<4;j++)
+//             {
+//                 System.out.print(nums[i][j] + " ");
+                
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+//jagged arrays : no of columns not fixed 
+
 class arrays
-{
+ {
     public static void main (String args[])
     {
-        int nums[][] = new int[3][4];
-        for (int i=0;i<3;i++)
-        {
-            for (int j= 0;j<4;j++)
-            {
-                nums[i][j]= (int)(Math.random() *10);
+        int nums[][] = new int[3][];
+        nums[0]= new int [3];
+        nums[0]= new int [3];
+        nums[0]= new int [3]; 
 
+        for ( int i =0;i<nums.length;i++)
+        {
+            for (int j =0;j<nums[i].length; j++)
+            {
+                nums[i][j] = (int)(Math.random() *10)
             }
         }
 
-        for (int i=0;i<3;i++)
+        for (int n[]: nums)
         {
-            for (int j= 0;j<4;j++)
+            for (m:n)
             {
-                System.out.print(nums[i][j] + " ");
-                
+                System.out.print(n + " ")
             }
-            System.out.println();
+            System.out.println()
         }
+        
     }
-}
+ }
