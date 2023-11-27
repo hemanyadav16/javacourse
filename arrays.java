82f932e3ccbcae4c59c53c9955583ba669da46a4
heman yadav16 <yadavheman16@gmail.com>
2023-11-27 18:45:47 +0530

    added

--- arrays.java
+++ arrays.java
@@ -86,31 +86,58 @@ class student
 
 class arrays
 {
-    student s1 = new student();
-    s1.rollno = 1;
-    s1.name = "hemzz";
-    s1.marks = 84;
-    
 
-    student s2 = new student();
-    s2.rollno= 1;
-    s2.name= "hemzz";
-    s2.marks= 87;
-    
-    student s3 = new student();
-    s3.rollno= 1;
-    s3.name= "hemzz";
-    s3.marks= 97;
-    
-    student students[] = new student[3];
-    students[0]= s1;
-    students[1]= s2;
-    students[2]= s3;
-
-    for (int i =0 ; i<students.length; i++)
+    public static void main (String args[])
     {
-        System.out.println(students[i].name + ":" + students[i].marks)
-    }
+        student s1 = new student();
+        s1.rollno = 1;
+        s1.name = "hemzz";
+        s1.marks = 84;
     
 
-} 
+        student s2 = new student();
+        s2.rollno= 1;
+        s2.name= "hemzz";
+        s2.marks= 87;
+    
+        student s3 = new student();
+        s3.rollno= 1;
+        s3.name= "hemzz";
+        s3.marks= 97;
+    
+        student students[] = new student[3];
+        students[0]= s1;
+        students[1]= s2;
+        students[2]= s3;
+
+        for (int i =0 ; i<students.length; i++)
+        {
+            System.out.println(students[i].name + ":" + students[i].marks);
+        }
+        //or
+        for (student stud: students)
+        {
+            System.out.println(stud.name + ":" + stud.marks);
+
+        }
+    
+
+    }
+}
+
+
+
+//enhanced for loop only for array as if now
+
+class arrays
+{
+    public static void main (String args[])
+    {
+        int nums[4] = {1,9,7,5}
+        for (int n : nums)
+        {
+            System.out.println(n)
+        }
+    }
+}
+
--- hello1.java
+++ hello1.java
@@ -1,43 +1,25 @@
-class hello
+// class hello
+// {
+//     public static void main (String args[])
+//     {
+//         int num =7;
+//         //num = num +1;
+//         //num+=1;
+//         //num++;
+//         //++num;
+//         int result = num++;   //fetches first and then increments next
+//         int result1 = ++num;  //increments first and then fetches
+//     }
+// }
+
+
+class hello1
 {
     public static void main (String args[])
     {
-        int num =7;
-        //num = num +1;
-        //num+=1;
-        //num++;
-        //++num;
-        int result = num++;   //fetches first and then increments next
-        int result1 = ++num;  //increments first and then fetches
+        int a= 10;
+        System.out.println(a++);
+        System.out.println(++a);
     }
-}
 
-
-
-// multidimensional array
-
-class arrays
-{
-    public static void main (String args[])
-    {
-        int nums[][] = new int[3][4];
-        for (int i=0;i<3;i++)
-        {
-            for (int j= 0;j<4;j++)
-            {
-                nums[i][j]= (int)(Math.random() *10);
-
-            }
-        }
-
-        for (int i=0;i<3;i++)
-        {
-            for (int j= 0;j<4;j++)
-            {
-                System.out.print(nums[i][j] + " ");
-                
-            }
-            System.out.println();
-        }
-    }
 }
