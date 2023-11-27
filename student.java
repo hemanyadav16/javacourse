a1240ec96022411af1452ff0a07ca7695cb787b2
heman yadav16 <yadavheman16@gmail.com>
2023-11-27 22:53:09 +0530

    variables

--- student.java
+++ student.java
@@ -1,10 +1,35 @@
+class mobile 
+{
+    String  brand;
+    int price;
+    String name;
+
+    public void show()
+    {
+        System.out.println(brand +":"+price+":"+name);
+    }
+
+}
+
 class student
 {
-    public static void main (String args[])
+    public static void main(String args[])
     {
-        StringBuffer sb = new StringBuffer("Heman");
-        sb.append(" yadav");
-        System.out.println(sb.charAt(3));
-        System.out.println(sb);
+        mobile obj1= new mobile();
+        obj1.brand= "apple";
+        obj1.price=1200;
+        obj1.name ="smartphone";
+
+        mobile obj2 = new mobile();
+        obj2.brand ="samsung";
+        obj2.price = 1000;
+        obj2.name = "smartphone";
+    
+
+       obj1.show();
+       obj2.show();
+
+
     }
+    
 }
