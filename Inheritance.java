a0859b929a6626248957b2b8845c300374703265
heman yadav16 <yadavheman16@gmail.com>
2023-12-12 19:44:05 +0530

    added to gib

---
+++ Inheritance.java
@@ -0,0 +1,86 @@
+/* inheritance is a important feature through which we can copy the feature of parent class  
+into chlid class
+
+class A    // parent class
+
+class B extends class A // child class here B copies feature of A */
+
+/*class Calc
+{
+    public int add(int n1,int n2)
+    {
+        return n1 + n2;
+    }
+    public int sub(int n1,int n2)
+    {
+        return n1 - n2;
+    }
+    
+}
+
+class Advcalc extends Calc
+{
+    public int multi(int n1,int n2)
+    {
+        return n1*n2;
+    }
+    public int div(int n1,int n2)
+    {
+        return n1/n2;
+    }
+}
+
+class Inheritance 
+{
+    public static void main ( String args[])
+    {
+        Advcalc obj = new Advcalc();
+        int r1 = obj.add(2,3);
+        int r2 = obj.sub(5,3);
+        int r3 = obj.add(6,3);
+        System.out.println( r1 + " " + r2 +" " +r3);
+    }
+}*/
+
+/* types of inheritance 
+singlr and multilevel inheritance
+
+class veryAdvCalc extends Advcal   // here veryAdv class copies features of Advclass and
+ Advclass copies features of Calc class in indirect form this is called  multilevel inheritance */
+
+
+
+
+ // method overriding
+
+// first preference will be given to the child or own class and then it will searchin parent class
+
+class Calc
+{
+    public int add(int n1,int n2)
+    {
+        return n1 + n2 ;
+    }
+}
+
+class AdvCalc extends Calc
+{
+    public int add(int n1,int n2)
+    {
+        return n1 + n2 + 1 ;
+    } 
+}
+
+
+public class Inheritance
+{
+    public static void main ( String args[])
+    {
+        AdvCalc obj = new AdvCalc();
+        int out = obj.add(3,6);
+        System.out.println(out);
+        
+    }
+}
+
+//here add method of advcalc overrides add method of calc class
--- constructor.java
+++ constructor.java
@@ -4,15 +4,21 @@
  * its name is same of class name
  * everytime when the object is created it calls the constructor*/
 
-class human 
+/*class human 
 {
     private int age;
     private String name;
     
-    public human()
+    public human()                  // default constructor
     {
         age = 12;
         name = "yadav";
+    }
+
+    public human(int a , String n)      // parameterised constructor
+    {
+        age = a;
+        name = n;
     }
     public int getage()
     {
@@ -41,11 +47,92 @@ class constructor
     public static void main (String args[])
     {
         human obj = new human();
+        human obj1 = new human(19,"heman");    //parameterised constructor call
         // obj.setage(11);
         // obj.setname("heman");
 
         System.out.println(obj.getname() + ":" + obj.getage());  // we can access the data like this 
+        System.out.println(obj1.getname() + ":" + obj1.getage());
+    
     }
 
 
+} */
+
+
+
+// super method 
+/* this is a special method it will their in every constructor either you define or not */
+
+/*import javax.swing.BoundedRangeModel;
+
+class human()
+{
+    public human()
+    {
+        super();
+        System.out.println("in human");
+    }
+
+    public human(int n)
+    {
+        super();
+        System.out.println("in human int ");
+    }
+}
+
+class demo extends human()
+{
+    public demo()
+    {
+        super();
+        System.out.println("in b ");
+    }
+    
+    public demo(int n)
+    {
+        super();
+        System.out.println("in b int");
+    }
+
+}
+class constructor
+{
+    public static void main( String args[] )
+    [
+        demo obj = new demo();
+    ]
+}*/
+
+
+
+
+// camel casing
+/*class and interface - Calc,Runable
+variable and method - marks , show()
+constants - PIE , BRAND 
+
+showMyMarks()
+MyData*/
+
+// ananymous object : an object with no reference name 
+
+class A 
+{
+    public A()
+    {
+        System.out.println("object created");
+    }
+    public void show()
+    {
+        System.out.println("in  a show");
+    }
+}
+
+class constructor
+{
+    public static void main (String args[])
+    {
+        new A().show();      // ananymous object it cant be used twice or more
+    }
 }
